88b3bde45462189f4dc9a492a4765b5cca787414
ninedev <master-vva@narod.ru>
2024-06-21 16:34:07 +0300

    init

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
   url: 'https://ninedev-i.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: '/docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
